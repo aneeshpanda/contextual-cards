c3ee29042d5e787820793cea9dcae17cda785d81
Aneesh Panda <aneeshpanda.ap@gmail.com>
2022-08-06 19:14:24 +0530

    feat: pull down to refresh

--- src/components/CardContainer/CardContainer.component.jsx
+++ src/components/CardContainer/CardContainer.component.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import PullToRefresh from "react-simple-pull-to-refresh";
 
 import CardRow from "../CardRow/CardRow.component";
 import Spinner from "../Spinner/Spinner.component";
@@ -10,6 +11,16 @@ import "./CardContainer.styles.css";
 const CardContainer = () => {
   const [cardsData, setCardsData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [refreshCall, setRefreshCall] = useState(false);
+
+  const handleRefresh = () => {
+    return new Promise((res) => {
+      setRefreshCall(true);
+      setTimeout(() => {
+        res(setRefreshCall(false));
+      }, 500);
+    });
+  };
 
   useEffect(() => {
     cardAPI()
@@ -25,12 +36,21 @@ const CardContainer = () => {
       });
   }, []);
   if (isLoading) return <Spinner />;
+
   return (
-    <div className="card-container">
-      {cardsData.map((cardRow, index) => {
-        return <CardRow cardRowData={cardRow} key={index} />;
-      })}
-    </div>
+    <PullToRefresh onRefresh={handleRefresh}>
+      <div className="card-container">
+        {cardsData.map((cardRow, index) => {
+          return (
+            <CardRow
+              cardRowData={cardRow}
+              refreshCall={refreshCall}
+              key={index}
+            />
+          );
+        })}
+      </div>
+    </PullToRefresh>
   );
 };
 
